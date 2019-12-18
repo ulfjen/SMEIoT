fa7f515c6a3ebf9a63e373a14728f22e3852655d
Erick Guan <fantasticfears@users.noreply.github.com>
2019-12-18 20:46:09 +0100

    build tmp folder in travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -7,5 +7,6 @@ REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 cd $REPO_ROOT && echo "Operating at root dir: $REPO_ROOT"
 source scripts/bootstrap.sh
 
+mkdir -p $TMP_BOOTSTRAP_DIR
 build_mosquitto
 mkdir -p out && cd out && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$(nproc)  && sudo make install
