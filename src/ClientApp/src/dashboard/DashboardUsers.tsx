7d3786577c3abff2e6ca799751245335ecb83168
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-10 20:17:14 +0100

    polish users page

--- src/ClientApp/src/dashboard/DashboardUsers.tsx
+++ src/ClientApp/src/dashboard/DashboardUsers.tsx
@@ -12,7 +12,7 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { GetDefaultApiConfig } from "../index";
-import { AdminUserApiModel, AdminUsersApi, AdminUserApiModelList } from "smeiot-client";
+import { AdminUserApiModel, AdminUsersApi, AdminUserApiModelList, AdminUsersApiApiAdminUsersGetRequest } from "smeiot-client";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
@@ -24,7 +24,7 @@ import moment from 'moment';
 import { Link, RouteComponentProps } from '@reach/router';
 import UserAvatarMenu from '../components/UserAvatarMenu';
 import { useAppCookie } from '../helpers/useCookie';
-import { defineMessages, useIntl } from 'react-intl';
+import { defineMessages, useIntl, FormattedMessage } from 'react-intl';
 import { useTitle } from 'react-use';
 import { FixedSizeList } from 'react-window';
 import InfiniteLoader from 'react-window-infinite-loader';
@@ -37,6 +37,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import useModal from "../helpers/useModal";
 import useMenu from "../helpers/useMenu";
+
+const FILTERS = ["Admin"]
 
 const styles = ({ palette, spacing, transitions, zIndex, mixins, breakpoints }: Theme) => createStyles({
   container: {
@@ -51,14 +53,8 @@ const styles = ({ palette, spacing, transitions, zIndex, mixins, breakpoints }: 
     },
     marginBottom: spacing(2)
   },
-  usersMenu: {
-  },
   usersMenuDeleteItem: {
     color: palette.error.main
-  },
-  item: {
-  },
-  card: {
   }
 });
 
@@ -85,16 +81,27 @@ const messages = defineMessages({
 
 const USERS_PER_REQ = 10;
 
-
+function eqSet<T>(as: Set<T>, bs: Set<T>) {
+  if (as.size !== bs.size) return false;
+  for (var a of as) if (!bs.has(a)) return false;
+  return true;
+}
 
 const ITEM_SIZE = 72;
 const FRAME_PADDING = 24;
 
-const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classes, navigate }) => {
+const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classes, navigate, location }) => {
+  if (location === undefined) { throw new Error("No location is found."); }
   const intl = useIntl();
   const appCookie = useAppCookie();
 
   useTitle(intl.formatMessage(messages.title));
+
+  const pathFilters = new URLSearchParams(location.search).getAll("roles").map(f => f.toLowerCase());
+  const [filters, setFilters] = React.useState<Record<string, boolean>>(FILTERS.reduce((map: Record<string, boolean>, f: string) => {
+    map[f] = pathFilters.indexOf(f.toLowerCase()) !== -1;
+    return map;
+  }, {}));
 
   const [users, setUsers] = React.useState<Array<AdminUserApiModel>>([]);
   const [hasNextPage, setHasNextPage] = React.useState<boolean>(true);
@@ -120,11 +127,28 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
     <List ref={ref} {...props} />
   )), []);
   const isUserLoaded = (index: number) => !hasNextPage || index < users.length;
-  const loadMoreUsers = (startIndex: number, stopIndex: number) => {
-    return new AdminUsersApi(GetDefaultApiConfig()).apiAdminUsersGet({
+  
+  const active = Object.entries(filters).filter(([_, enabled]) => enabled).map(([k, v]) => k.toLowerCase());
+
+  React.useEffect(() => {
+    if (!eqSet<string>(new Set(active), new Set(pathFilters))) {
+      let path = location.pathname;
+      if (active.length !== 0) {
+        path = location.pathname + "?"+active.map(f => `roles=${f}`).join("&");
+      }
+      window.location.href = path;
+    }
+  }, [filters]);
+
+  const loadMoreUsers = React.useCallback((startIndex: number, stopIndex: number) => {
+    const req: AdminUsersApiApiAdminUsersGetRequest = {
       offset: startIndex, // we only render things when scroll down so no need to be extra fancy
       limit: USERS_PER_REQ
-    }).then((result: AdminUserApiModelList) => {
+    };
+    if (active && active.length > 0) {
+      req["roles"] = active;
+    }
+    return new AdminUsersApi(GetDefaultApiConfig()).apiAdminUsersGet(req).then((result: AdminUserApiModelList) => {
       if (!result.users) {
         return;
       }
@@ -135,14 +159,30 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
       if (result.total && newUsers.length >= result.total) {
         setHasNextPage(false);
       }
-      console.log("setting newUser", newUsers, result);
       return result;
     });
-  }
+  }, [filters, closeMenu, users]);
+
   // for loading indicator
   const userCount = hasNextPage ? users.length + 1 : users.length;
 
-  console.log("menu anchorEl", menuAnchorEl);
+  const flipFilter = (filter: string, value: boolean) => {
+    const inc: Record<string, boolean> = {};
+    inc[filter] = value;
+    
+    setFilters({ ...filters, ...inc });
+  };
+  const selectFilter = (filter: string) => flipFilter(filter, true);
+  const unselectFilter = (filter: string) => flipFilter(filter, false);
+  const filterSelected = (filter: string) => filters[filter];
+  const renderFilter = (label: string, index: number) => {
+    return <Chip key={index} onClick={() => selectFilter(label)} onDelete={() => unselectFilter(label)} color={filterSelected(label) ? "primary" : "default"} label={label} />;
+  };
+
+  const renderFilters = () => {
+    return FILTERS.map((name, index) => renderFilter(name, index));
+  }
+
 
   const initialHeight = userCount * ITEM_SIZE;
   const containerRef = React.createRef<HTMLElement>();
@@ -163,14 +203,13 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
     index: number,
     style: React.CSSProperties
   }) => {
-    console.log("rendering", index);
     return index === users.length ? <ListItem key={-1} style={style}>
-        <ListItemAvatar>
-          <Skeleton variant="circle" width={40} height={40} />
-        </ListItemAvatar>
+      <ListItemAvatar>
+        <Skeleton variant="circle" width={40} height={40} />
+      </ListItemAvatar>
 
-        <ListItemText disableTypography primary={<Skeleton variant="rect" width={200} height={17} />} secondary={<Skeleton variant="text" />} />
-      </ListItem> :
+      <ListItemText disableTypography primary={<Skeleton variant="rect" width={200} height={17} />} secondary={<Skeleton variant="text" />} />
+    </ListItem> :
       <ListItem key={index} style={style}>
         <ListItemAvatar>
           <Avatar>
@@ -179,7 +218,7 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
         </ListItemAvatar>
 
         <ListItemText primary={users[index].userName} secondary={intl.formatMessage(messages.seen, { seen: moment(users[index].lastSeenAt).fromNow() })} />
-        <IconButton edge="end" onClick={(event) => {console.log("opening menu with", users[index].userName); openMenu(event.currentTarget, users[index].userName)}}>
+        <IconButton edge="end" onClick={(event) => { openMenu(event.currentTarget, users[index].userName) }}>
           <MoreVertIcon />
         </IconButton>
       </ListItem>;
@@ -195,9 +234,9 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
       <Container maxWidth="lg" className={classes.container}>
         <Paper className={classes.filterBar}>
           <Typography>Show:</Typography>
-          <Chip label="Admin" />
+          {renderFilters()}
         </Paper>
-        <Paper ref={measureRef} className={classes.card}>
+        <Paper ref={measureRef}>
           <InfiniteLoader
             isItemLoaded={isUserLoaded}
             itemCount={userCount}
@@ -223,11 +262,22 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
         <Menu
           anchorEl={menuAnchorEl}
           open={menuOpen}
-          className={classes.usersMenu}
           onClose={closeMenu}
         >
-          <MenuItem onClick={closeMenu} to={`/dashboard/users/${menuUserName}`} component={Link}>Edit</MenuItem>
-          <MenuItem className={classes.usersMenuDeleteItem} onClick={handleDelete}>Delete</MenuItem>
+          <MenuItem onClick={closeMenu} to={`/dashboard/users/${menuUserName}`} component={Link}>
+            <FormattedMessage
+              id="dashboard.users.index.list.actions.edit"
+              description="The text on edit user menu"
+              defaultMessage="Edit"
+            />
+          </MenuItem>
+          <MenuItem className={classes.usersMenuDeleteItem} onClick={handleDelete}>
+            <FormattedMessage
+              id="dashboard.users.index.list.actions.delete"
+              description="The text on delete user menu"
+              defaultMessage="Delete"
+            />
+          </MenuItem>
         </Menu>
         <Dialog
           open={dialogOpen}
@@ -235,18 +285,37 @@ const _DashboardUsers: React.FunctionComponent<IDashboardUsersProps> = ({ classe
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description"
         >
-          <DialogTitle>Delete account {dialogUserName}?</DialogTitle>
+          <DialogTitle>
+            <FormattedMessage
+              id="dashboard.users.index.delete.title"
+              description="The text on delete user dialog"
+              defaultMessage="Delete account {name}?"
+              values={{name: dialogUserName}}
+            />
+          </DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Delete the user will disable its access to the system. And this may result in unexpected behaviour.
+              <FormattedMessage
+                id="dashboard.users.index.delete.instruction"
+                description="The text on delete user dialog"
+                defaultMessage="Delete the user will disable its access to the system. And this may result in unexpected behaviour."
+              />
             </DialogContentText>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleDialogClose} color="primary">
-              Cancel
+              <FormattedMessage
+                id="dashboard.users.index.delete.actions.cancel"
+                description="The text on delete user dialog"
+                defaultMessage="Cancel"
+              />
             </Button>
             <Button onClick={handleDeleteClose} color="primary" autoFocus>
-              Delete
+              <FormattedMessage
+                id="dashboard.users.index.delete.actions.delete"
+                description="The text on delete user dialog"
+                defaultMessage="Delete"
+              />
             </Button>
           </DialogActions>
         </Dialog>
