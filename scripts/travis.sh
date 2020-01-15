62b3071ed461430d8dae65f0b4b1ff8d2d427c3d
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-15 11:02:05 +0100

    travis: go back to the root

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -13,3 +13,4 @@ sudo apt-get install -y build-essential \
     cmake
 build_mosquitto
 cd $REPO_ROOT && mkdir -p out && cd out && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$(nproc)  && sudo make install
+cd $REPO_ROOT
