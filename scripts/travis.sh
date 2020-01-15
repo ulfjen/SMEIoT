f9d0ece130ba49ede14bdb96bf2c171ece71def2
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-15 10:57:03 +0100

    fix travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -12,4 +12,4 @@ sudo apt-get install -y build-essential \
     libc-ares-dev \
     cmake
 build_mosquitto
-cd $REPO_ROOT && mkdir -p out && cd out && sudo chown -R travis:cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$(nproc)  && sudo make install
+cd $REPO_ROOT && mkdir -p out && cd out && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$(nproc)  && sudo make install
