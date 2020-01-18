4a5499897f5195d2e974b86af1722811a1fbf8ca
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-18 23:37:30 +0100

    travis: fix missing build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -13,5 +13,7 @@ sudo apt-get install -y build-essential \
     cmake
 install_dotnet_ef
 setup_test_db
+build_mosquitto
 cd $REPO_ROOT && mkdir -p out && cd out && cmake .. -DCMAKE_BUILD_TYPE=Debug && make -j$(nproc)  && sudo make install
+cd $REPO_ROOT/src/ClientApp/vendor/smeiot-client && npm install && npm run build
 cd $REPO_ROOT/src/ClientApp && npm install && npm run build && cp -r build $REPO_ROOT/src/SMEIoT.Web/wwwroot
