e67e85da6ce6da39b021a00f407e43a5aa392bb0
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-21 16:29:53 +0100

    add statistics on dashboard index

--- src/ClientApp/src/dashboard/DashboardIndex.tsx
+++ src/ClientApp/src/dashboard/DashboardIndex.tsx
@@ -14,8 +14,13 @@ import { Link as ReachLink, RouteComponentProps } from '@reach/router';
 import UserAvatarMenu from '../components/UserAvatarMenu';
 import { useAppCookie } from '../helpers/useCookie';
 import BasicBrokerCard from './BasicBrokerCard';
+import { FormattedMessage, defineMessages, useIntl } from 'react-intl';
+import useInterval from '../helpers/useInterval';
+import { DashboardApi, SystemHighlightsApiModel } from 'smeiot-client';
+import { GetDefaultApiConfig } from '..';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-const styles = ({ palette, spacing, transitions, zIndex, mixins, breakpoints }: Theme) => createStyles({
+const styles = ({ palette, spacing }: Theme) => createStyles({
   container: {
   },
   paper: {
@@ -35,11 +40,34 @@ const styles = ({ palette, spacing, transitions, zIndex, mixins, breakpoints }: 
 export interface IDashboardIndexProps extends RouteComponentProps, WithStyles<typeof styles> {
 }
 
+const messages = defineMessages({
+  title: {
+    id: "dashboard.index.title",
+    description: "Dashboard title",
+    defaultMessage: "Dashboard"
+  }
+})
+
 const _DashboardIndex: React.FunctionComponent<IDashboardIndexProps> = ({ classes, navigate }) => {
+  const intl = useIntl();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   const appCookie = useAppCookie();
-  return <DashboardFrame title="Dashboard" direction="ltr"
+
+  const [highlights, setHighlights] = React.useState<SystemHighlightsApiModel>();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  useInterval(async () => {
+    setLoading(true);
+    const api = new DashboardApi(GetDefaultApiConfig());
+    await api.apiDashboardGet().then((res) => {
+      setHighlights(res);
+      return res;
+    }).finally(() => {
+      setLoading(false);
+    });
+  }, 30000, true);
+
+  return <DashboardFrame title={intl.formatMessage(messages.title)} direction="ltr"
     drawer
     toolbarRight={<UserAvatarMenu appCookie={appCookie} navigate={navigate}/>}
     content={
@@ -48,40 +76,86 @@ const _DashboardIndex: React.FunctionComponent<IDashboardIndexProps> = ({ classe
           {/* Sensor stats */}
           <Grid item xs={12} md={8} lg={9}>
             <Paper className={fixedHeightPaper}>
-              <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                Sensors
-              </Typography>
-              <Typography component="p" variant="h4">
-                1
-              </Typography>
+              {loading ? <Skeleton variant="rect" width={120} height={30} /> : <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                <FormattedMessage
+                  id="dashboard.index.sensors.title"
+                  description="The title on dashboard sensor card"
+                  defaultMessage="Sensors"
+                /> 
+                </Typography>
+              }
+              {loading ?
+                <div><Skeleton variant="text" /><Skeleton variant="text" /><Skeleton variant="text" /></div> :
+                <Typography component="p" variant="h4">
+                  {!loading &&
+                    <FormattedMessage
+                      id="dashboard.index.sensors.first"
+                      description="The fisrt text on dashboard sensor card"
+                      defaultMessage="{connectedCount} connected of {count}"
+                      values={{ count: highlights && highlights.sensorCount, connectedCount: highlights && highlights.connectedSensorCount}}
+                    /> 
+                  }
+                </Typography>
+              }
               <Typography color="textSecondary" className={classes.context}>
+                {!loading &&
+                  <FormattedMessage
+                    id="dashboard.index.sensors.second"
+                    description="The second text on dashboard sensor card"
+                    defaultMessage="among {count} device ({connectedCount} connected)"
+                    values={{ count: highlights && highlights.deviceCount, connectedCount: highlights && highlights.connectedDeviceCount}}
+                  /> 
+                }
               </Typography>
-              <div>
-                <Link color="primary" to="/dashboard/sensors" component={ReachLink}>
-                  View sensors
-                  </Link>
-              </div>
+              {loading ? <Skeleton variant="rect" width={150} height={20} /> : <div>
+                <Link color="primary" to="sensors" component={ReachLink}>
+                  <FormattedMessage
+                    id="dashboard.index.sensors.link"
+                    description="The link on dashboard sensor card"
+                    defaultMessage="View sensors"
+                  /> 
+                </Link>
+              </div>}
             </Paper>
           </Grid>
           {/* User stats */}
           <Grid item xs={12} md={4} lg={3}>
             <Paper className={fixedHeightPaper}>
-              <Typography component="h2" variant="h6" color="primary" gutterBottom>
-                Users
+              {loading ? <Skeleton variant="rect" width={120} height={30} /> : <Typography component="h2" variant="h6" color="primary" gutterBottom>
+                <FormattedMessage
+                  id="dashboard.index.users.title"
+                  description="The title on dashboard users card"
+                  defaultMessage="Users"
+                /> 
                 </Typography>
-              <Typography component="p" variant="h4">
-                1
+              }
+              {loading ?
+                <div><Skeleton variant="text" /><Skeleton variant="text" /><Skeleton variant="text" /></div> :
+                <Typography component="p" variant="h4">
+                  {!loading && highlights && highlights.userCount}
                 </Typography>
+              }
               <Typography color="textSecondary" className={classes.context}>
+                {!loading &&
+                    <FormattedMessage
+                      id="dashboard.index.users.second"
+                      description="The second text on dashboard sensor card"
+                      defaultMessage="{count} admin"
+                      values={{ count: highlights && highlights.adminCount }}
+                    /> 
+                  }
               </Typography>
-              <div>
-                <Link color="primary" to="/dashboard/sensors" component={ReachLink}>
-                  View users
-                  </Link>
-              </div>
+              {loading ? <Skeleton variant="rect" width={150} height={20} /> : <div>
+                <Link color="primary" to="users" component={ReachLink}>
+                  <FormattedMessage
+                    id="dashboard.index.users.link"
+                    description="The link on dashboard users card"
+                    defaultMessage="View users"
+                  /> 
+                </Link>
+              </div>}
             </Paper>
           </Grid>
-          {/* System stats */}
           <Grid item xs={12}>
             <BasicBrokerCard />
           </Grid>
