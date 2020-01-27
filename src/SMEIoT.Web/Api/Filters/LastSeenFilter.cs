2cb07a9cc3c61da1ff3136e516d9d8866e37f176
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-27 11:47:30 +0100

    don't starts the job if last seen at is not expired

--- src/SMEIoT.Web/Api/Filters/LastSeenFilter.cs
+++ src/SMEIoT.Web/Api/Filters/LastSeenFilter.cs
@@ -1,13 +1,10 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Filters;
-using SMEIoT.Web.Api.V1;
 using Hangfire;
 using NodaTime;
 using Microsoft.AspNetCore.Identity;
 using SMEIoT.Core.Entities;
 using SMEIoT.Core.Interfaces;
-using System;
-using System.Collections.Generic;
 
 namespace SMEIoT.Web.Api.Filters
 {
@@ -26,9 +23,10 @@ namespace SMEIoT.Web.Api.Filters
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
       var user = await _userManager.GetUserAsync(context.HttpContext.User);
-      if (user != null)
+      var now = _clock.GetCurrentInstant();
+      if (user != null && user.LastSeenAt + Duration.FromMinutes(2) < now)
       {
-        BackgroundJob.Enqueue<IUserProfileService>(service => service.UpdateUserLastSeenAsync(user.Id, _clock.GetCurrentInstant().ToDateTimeUtc()));
+        BackgroundJob.Enqueue<IUserProfileService>(service => service.UpdateUserLastSeenAsync(user.Id, now.ToDateTimeUtc()));
       }
       await next();
     }
