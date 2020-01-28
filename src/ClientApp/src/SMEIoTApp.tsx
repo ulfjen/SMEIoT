200873e2ec1ca40d34e7897bec369bbab7eb0212
Erick Guan <fantasticfears@users.noreply.github.com>
2020-01-28 10:24:42 +0100

    inject mini profiler with hooks

--- src/ClientApp/src/SMEIoTApp.tsx
+++ src/ClientApp/src/SMEIoTApp.tsx
@@ -9,6 +9,7 @@ import { palette, typography } from "./theme";
 import RootApp from "./RootApp";
 import EnMessages from "./locales/en.json";
 import DashboardApp from "./DashboardApp";
+import { useAppCookie } from "./helpers/useCookie";
 
 const SMEIoTApp: React.FunctionComponent = () => {
   let locale = "en";
@@ -18,6 +19,36 @@ const SMEIoTApp: React.FunctionComponent = () => {
     default:
       messages = EnMessages;
   }
+  
+  const appCookie = useAppCookie();
+  React.useEffect(() => {
+    if (!appCookie.admin) {
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.id = "mini-profiler";
+    script.src = "/profiler/includes.min.js";
+    script.async = true;
+    // a hack so that we get 404 instead of 500
+    // this does not get to a security problem anyhow but it's better with a 4xx.
+    script.dataset.ids = "00000000-0000-0000-0000-000000000000";
+    script.dataset.version = "4.1.0+202001271656";
+    script.dataset.path = "/profiler/";
+    script.dataset.position = "BottomLeft";
+    script.dataset.authorized = "true"
+    script.dataset.maxTraces = "10";
+    script.dataset.toggleShortcut = "Alt+P";
+    script.dataset.trivialMilliseconds = "2.0";
+    script.dataset.ignoredDuplicateExecuteTypes = "Open,OpenAsync,Close,CloseAsync";
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    }
+  }, [appCookie]);
+
 
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
